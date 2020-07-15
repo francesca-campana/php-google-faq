3ee933d525f3aac06ac2fd9d96008c8899265dc0
francesca-campana <franx3rox@gmail.com>
2020-07-15 18:46:38 +0200

    commenti

--- index.php
+++ index.php
@@ -4,22 +4,21 @@ $faqs = [
   [
     'question'=> 'How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?',
     'answer' => 'The recent ruling by the Court of Justice of the European Union has profound consequences
-                for search engines in Europe. T
-                he court found that certain users have the right to ask search engines like Google to remove
+                for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove
                 results for queries that include the person\'s name.
-                To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.
+                To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.' . '<br>' . '<br>' . '
                 Since this ruling was published on 13 May 2014, we\'ve been working round the clock to comply.
                 This is a complicated process because we need to assess each
-                individual request and balance the rights of the individual to control his or her personal data with public\'s right to know and distribute information.
+                individual request and balance the rights of the individual to control his or her personal data with public\'s right to know and distribute information.' . '<br>' . '<br>' . '
                 If you have a removal request, please fill out this webform. You\'ll receive an automatic reply confirming that we have received your request. We will then
                 assess your case—please note this may take some time because we have already received many such requests. In evaluating your request, we will look at whether
                 the results include outdated information about your private life. We\'ll also look at whether there\'s a public interest in the information remaining in our
                 search results—for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official
                 (elected or unelected). These are difficult judgements and as a private organization, we may not be in a good position to decide on your case. If you disagree
-                with our decision you can contact your local DPA.
+                with our decision you can contact your local DPA.'. '<br>' . '<br>' . '
                 We look forward to working closely with data protection authorities and others over the coming months as we refine our approach.
                 The CJEU\'s ruling constitutes a significant change for search engines. While we are concerned about its impact, we also believe
-                it\'s important to respect the Court\'s judgment and are working hard to devise a process that complies with the law.
+                it\'s important to respect the Court\'s judgment and are working hard to devise a process that complies with the law.' . '<br>' . '<br>' . '
                 When you search for a name, you may see a notice that says that results may have been modified in accordance with data protection
                 law in Europe. We’re showing this notice in Europe when a user searches for most names, not just pages that have been affected by a removal.'
   ],
@@ -27,15 +26,15 @@ $faqs = [
     'question' => 'How does Google protect my privacy and keep my information secure?',
     'answer' => 'We know security and privacy are important to you – and they are important to us,
                 too. We make it a priority to provide strong security and give you confidence that
-                your information is safe and accessible when you need it.
+                your information is safe and accessible when you need it.' . '<br>' . '<br>' . '
                 We’re constantly working to ensure strong security, protect your privacy, and make Google
                 even more effective and efficient for you.
                 We spend hundreds of millions of dollars every year on security, and employ world-renowned
                 experts in data security to keep your information safe.
                 We also built easy-to-use privacy and security tools like Google Dashboard, 2-step verification and Ads Settings.
-                So when it comes to the information you share with Google, you’re in control.
+                So when it comes to the information you share with Google, you’re in control.' . '<br>' . '<br>' . '
                 You can learn more about safety and security online, including how to protect yourself and
-                your family online, at the Google Safety Center.
+                your family online, at the Google Safety Center.' . '<br>' . '<br>' . '
                 Learn more about how we keep your personal information private and safe — and put you in control.'
   ],
   [
@@ -72,16 +71,21 @@ $faqs = [
   </head>
   <body>
     <div class="wrapper">
+      <!-- Header -->
       <header>
+        <!-- Navbar -->
         <div class="nav-bar">
+          <!-- Logo -->
           <div class="logo">
             <div class="google-logo">
               <img src="img/googlelogo.png" alt="google logo">
-
             </div>
             <span>Privacy & Terms</span>
           </div>
+          <!-- Fine Logo -->
+          <!-- Nav Menu -->
           <div class="nav-menu">
+            <!-- Lista Menu -->
             <ul>
               <li>
                 <a href="#">Overview</a>
@@ -101,35 +105,39 @@ $faqs = [
 
               </li>
             </ul>
+            <!-- Fine Lista Menu -->
+            <!-- Google Account -->
             <div class="google-account">
               <div class="photo-account">
                 <img src="img/leo.png" alt="foto account">
               </div>
               <span>franx3rox@gmail.com</span>
-
             </div>
+            <!-- Fine Google Account -->
           </div>
-
+          <!-- Fine Nav Menu -->
         </div>
+        <!-- Fine Navbar -->
       </header>
+      <!-- Fine Header -->
+      <!-- Main -->
       <main>
+        <!-- Container -->
         <div class="container">
 
           <?php  foreach($faqs as $faq) { ?>
+            <!-- Faq -->
             <div class="faq">
+              <!--  -->
             <h2> <?php echo $faq['question']; ?></h2>
-            <span><?php echo $faq['answer'] . '<br>'; ?></span>
+            <span><?php echo $faq['answer']; ?></span>
             </div>
           <?php } ?>
-
-
-
+          <!-- Fine Faq -->
         </div>
-
-
-
-
+        <!-- Fine Container -->
       </main>
+      <!-- Fine Main -->
     </div>
 
   </body>
